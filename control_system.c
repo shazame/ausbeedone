a26047138bf4c7177d1277190ece6533dd5d54fe
David Bitonneau <david.bitonneau@gmail.com>
2014-05-26 23:33:17 +0200

    control system: pid distance tuned

--- control_system.c
+++ control_system.c
@@ -61,7 +61,7 @@ static void control_system_init_motors(struct control_system *am)
 
 static void control_system_init_distance_angle(struct control_system *am)
 {
-  ausbee_pid_init(&(am->pid_distance), 1, 0.3, 2.0);
+  ausbee_pid_init(&(am->pid_distance), 2.5, 0.5, 0.5);
   ausbee_pid_init(&(am->pid_angle),    1, 0.01, 0);
 
   ausbee_pid_set_output_range(&(am->pid_distance), -100, 100);
