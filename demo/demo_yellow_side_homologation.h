1deaf28f49898e7a350e1ad545d8782c117ddd78
Vincent Faure <vfaure@enseirb-matmeca.fr>
2014-05-28 23:40:49 +0200

    homologation codes

---
+++ demo/demo_yellow_side_homologation.h
@@ -0,0 +1,8 @@
+#ifndef DEMO_YELLOW_SIDE_HOMOLOGATION
+#define DEMO_YELLOW_SIDE_HOMOLOGATION
+
+#include "../trajectory_manager.h"
+
+void demo_yellow_side_homologation_start(struct trajectory_manager *t);
+
+#endif /* DEMO_YELLOW_SIDE_HOMOLOGATION */
