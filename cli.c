937c993e27201b35da9cb8dade1642aabfb599ef
David Bitonneau <david.bitonneau@gmail.com>
2015-05-09 16:33:25 +0200

    cli new feature: char deletion

--- cli.c
+++ cli.c
@@ -14,12 +14,239 @@
 #include "demo/demo_pid.h"
 
 void cli_task(void *);
+void cli_execute(char *cmd, struct trajectory_manager *t, uint8_t argc, char **argv);
 
 void cli_start(struct trajectory_manager *t)
 {
   xTaskCreate(cli_task, (const signed char *)"CLI", 200, (void *)t, 1, NULL );
 }
 
+static void cli_getline(char *buff)
+{
+  int c = 0;
+  int i = 0;
+
+  memset(buff, 0, CLI_BUFFER_SIZE * sizeof(char));
+
+  while (((c = getchar()) != EOF) && ((char)c != CLI_END_CHAR)) {
+    if ((char)c == CLI_DEL_CHAR) {
+      if (i > 0) {
+        buff[--i] = 0;
+        printf("\b \b");
+      }
+    }
+    else if (i < CLI_BUFFER_SIZE-1) {
+      buff[i++] = (char)c;
+      printf("%c", (char)c);
+    }
+  }
+  printf("\r\n");
+}
+
+void cli_task(void *data)
+{
+  struct trajectory_manager *t = (struct trajectory_manager *)data;
+
+  char cli_buffer[CLI_BUFFER_SIZE];
+  uint8_t argc = 0;
+  char *cmd;
+  char *argv[CLI_MAX_ARGS+1];
+
+  for (;;) {
+    printf("$ ");
+    cli_getline(cli_buffer);
+
+    // CLI buffer tokenization
+    cmd = strtok(cli_buffer, CLI_DELIMITER);
+    for (argc = 0; cmd != NULL; argc++) {
+      argv[argc] = strtok(NULL, CLI_DELIMITER);
+      if (argv[argc] == NULL || argc >= CLI_MAX_ARGS)
+        break;
+    }
+
+    if (cmd == NULL) {
+      printf("Read line is empty. Type 'help' for help.\r\n");
+    }
+    else {
+      cli_execute(cmd, t, argc, argv);
+    }
+
+    vTaskDelay(1 / portTICK_RATE_MS);
+  }
+}
+
+static int8_t cli_streq(char *str1, char*str2)
+{
+  return !strncmp(str1, str2, CLI_BUFFER_SIZE);
+}
+
+void cli_execute_goto_d(struct trajectory_manager *t, uint8_t argc, char **argv)
+{
+  if (argc < 1) {
+    printf("Error: the command 'd' needs 1 argument.\r\n");
+  }
+
+  float distance = strtof(argv[0], NULL);
+  trajectory_goto_d_mm(t, distance);
+  printf("Distance: %f\r\n", (double)distance);
+}
+
+void cli_execute_goto_a(struct trajectory_manager *t, uint8_t argc, char **argv)
+{
+  if (argc < 1) {
+    printf("Error: the command 'a' needs 1 argument.\r\n");
+  }
+
+  float angle = strtof(argv[0], NULL);
+  trajectory_goto_a_rel_deg(t, angle);
+  printf("Angle: %f\r\n", (double)angle);
+}
+
+void cli_execute_get(struct trajectory_manager *t, uint8_t argc, char **argv)
+{
+  if (argc < 1) {
+    printf("Error: the command 'get' needs at least 1 argument.\r\n");
+  }
+
+  uint8_t i = 0;
+  for (; i < argc; i++) {
+    if (cli_streq(argv[i], "x")) {
+      printf("Robot x mm: %f\r\n", (double)position_get_x_mm());
+    }
+    else if (cli_streq(argv[i], "y")) {
+      printf("Robot y mm: %f\r\n", (double)position_get_y_mm());
+    }
+    else if (cli_streq(argv[i], "a")) {
+      printf("Robot angle deg: %f\r\n", (double)position_get_angle_deg(NULL));
+    }
+    else if (cli_streq(argv[i], "d")) {
+      printf("Robot distance mm: %f\r\n", (double)position_get_distance_mm(NULL));
+    }
+    else if (cli_streq(argv[i], "encr")) {
+      printf("Right encoder value: %f\r\n", (double)position_get_right_encoder(NULL));
+    }
+    else if (cli_streq(argv[i], "encl")) {
+      printf("Left encoder value: %f\r\n", (double)position_get_left_encoder(NULL));
+    }
+    else if (cli_streq(argv[i], "cur_id")) {
+      printf("Position manager cur_id: %"PRId32"\r\n", trajectory_get_cur_id(t));
+    }
+    else if (cli_streq(argv[i], "last_id")) {
+      printf("Position manager last_id: %"PRId32"\r\n", trajectory_get_last_id(t));
+    }
+    else if (cli_streq(argv[i], "pid_dump")) {
+      printf("Distance PID: %f, %f, %f\r\n", (double)ausbee_pid_get_kp(&(t->cs->pid_distance)),
+          (double)ausbee_pid_get_ki(&(t->cs->pid_distance)),
+          (double)ausbee_pid_get_kd(&(t->cs->pid_distance)));
+      printf("Angle PID:    %f, %f, %f\r\n", (double)ausbee_pid_get_kp(&(t->cs->pid_angle)),
+          (double)ausbee_pid_get_ki(&(t->cs->pid_angle)),
+          (double)ausbee_pid_get_kd(&(t->cs->pid_angle)));
+    }
+    else {
+      printf("Invalid argument '%s'.\r\n", argv[i]);
+    }
+  }
+}
+
+void cli_execute_set(uint8_t argc, char **argv)
+{
+}
+
+void cli_execute_start(uint8_t argc, char **argv)
+{
+}
+
+void cli_execute_stop(uint8_t argc, char **argv)
+{
+}
+
+void cli_execute_call(uint8_t argc, char **argv)
+{
+}
+
+void cli_execute_help()
+{
+  printf("Help:\r\n");
+  printf("  Available commands are:\r\n");
+  printf("  d <value>: Go forward/backward with the specified distance in mm.\r\n");
+  printf("  a <value>: Rotate with the specified angle in degrees.\r\n");
+  printf("  start/stop <arg>: Start/stop a task.\r\n");
+  printf("             <arg> can be one of:\r\n");
+  //printf("             ys: Yellow strategy task.\r\n");
+  //printf("             rs: Red strategy task.\r\n");
+  //printf("             pid: PID test task.\r\n");
+  printf("  get <arg1> <arg2> ...: Print internal values.\r\n");
+  printf("             <argx> can be one of:\r\n");
+  printf("             x:        print robot's x position.\r\n");
+  printf("             y:        print robot's y position.\r\n");
+  printf("             a:        print robot's angle.\r\n");
+  printf("             d:        print robot's distance.\r\n");
+  printf("             encl:    print left encoder's value.\r\n");
+  printf("             encr:    print right encoder's value.\r\n");
+  printf("             cur_id:   print position manager's current point id.\r\n");
+  printf("             last_id:  print position manager's last point id.\r\n");
+  printf("             pid_dump: print PID.\r\n");
+  printf("  set <arg> <value>: Set internal value.\r\n");
+  //printf("             <value> should be a float.\r\n");
+  //printf("             <arg> can be one of:\r\n");
+  //printf("             speed_high:   set highest translation and rotation speed.\r\n");
+  //printf("             speed_medium: set medium translation and rotation speed.\r\n");
+  //printf("             speed_low:    set low translation and rotation speed.\r\n");
+  //printf("             speed :       set translation and rotation speed ratio to value (0 <= value <= 1).\r\n");
+  //printf("             pid_d_P :     set distance PID proportional value.\r\n");
+  //printf("             pid_d_I :     set distance PID integral value.\r\n");
+  //printf("             pid_d_D :     set distance PID derivative value.\r\n");
+  //printf("             pid_a_P :     set angle PID proportional value.\r\n");
+  //printf("             pid_a_I :     set angle PID integral value.\r\n");
+  //printf("             pid_a_D :     set angle PID derivative value.\r\n");
+  //printf("             axle_track :  set axle track in mm.\r\n");
+  //printf("  m <arg> <arg2> : move an actuator\r\n");
+  //printf("             <arg> can be one of: \r\n");
+  //printf("             arm_l: left_arm \r\n");
+  //printf("             arm_r: right_arm \r\n");
+  //printf("                <arg2> can be one of: \r\n");
+  //printf("                close: close the arm \r\n");
+  //printf("                open: open the arm \r\n");
+  //printf("             paint_r: right_paint \r\n");
+  //printf("             paint_l: left_paint \r\n");
+  //printf("                <arg2> can be one of: \r\n");
+  //printf("                put: put the paint on the \"fresque\" \r\n");
+  //printf("                release: release the paint on the \"fresque\" \r\n");
+  printf("  help: Display this help.\r\n");
+}
+
+void cli_execute(char *cmd, struct trajectory_manager *t, uint8_t argc, char **argv)
+{
+  if (cli_streq(cmd, "d")) {          // Translate by given distance
+    cli_execute_goto_d(t, argc, argv);
+  }
+  else if (cli_streq(cmd, "a")) {     // Rotate by given angle
+    cli_execute_goto_a(t, argc, argv);
+  }
+  else if (cli_streq(cmd, "get")) {   // get a variable
+    cli_execute_get(t, argc, argv);
+  }
+  else if (cli_streq(cmd, "set")) {   // set a variable
+    cli_execute_set(argc, argv);
+  }
+  else if (cli_streq(cmd, "start")) { // start a task
+    cli_execute_start(argc, argv);
+  }
+  else if (cli_streq(cmd, "stop")) {  // stop a task
+    cli_execute_stop(argc, argv);
+  }
+  else if (cli_streq(cmd, "call")) {  // call a function
+    cli_execute_call(argc, argv);
+  }
+  else if (cli_streq(cmd, "help")) {  // call a function
+    cli_execute_help();
+  }
+  else {
+      printf("Unknown command '%s'. Type 'help' for help.\r\n", cmd);
+  }
+}
+
+/*
 static int cli_getchar(void)
 {
   int c = getchar();
@@ -32,28 +259,6 @@ static int cli_getchar(void)
   }
 
   return c;
-}
-
-static void cli_getline(char *buff, uint8_t line_length)
-{
-  int c = 0;
-  int i = 0;
-
-  memset(buff, 0, line_length * sizeof(char));
-
-  while (((c = getchar()) != EOF) && ((char)c != CLI_END_CHAR)) {
-    if ((char)c == CLI_DEL_CHAR) {
-      if (i > 0) {
-        buff[--i] = 0;
-        printf("\b \b");
-      }
-    }
-    else if (i < line_length-1) {
-      buff[i++] = (char)c;
-      printf("%c", (char)c);
-    }
-  }
-  printf("\r\n");
 }
 
 static void cli_getstr(char *buff, uint8_t str_length)
@@ -387,3 +592,4 @@ void cli_task(void *data)
     vTaskDelay(1 / portTICK_RATE_MS);
   }
 }
+*/
--- cli.h
+++ cli.h
@@ -3,9 +3,12 @@
 
 #include "trajectory_manager.h"
 
+#define CLI_BUFFER_SIZE  80
+#define CLI_MAX_ARGS     8
+
 #define CLI_END_CHAR  '\r'
 #define CLI_DEL_CHAR  127
-#define CLI_DELIMITER ' '
+#define CLI_DELIMITER " "
 
 void cli_start(struct trajectory_manager *);
 
