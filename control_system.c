b9954ccd8130f17278fa251fb9f823e6a17d5c0f
David Bitonneau <david.bitonneau@gmail.com>
2015-05-11 01:09:09 +0200

    Control system tuning: better PID parameters

--- control_system.c
+++ control_system.c
@@ -72,8 +72,8 @@ static void control_system_init_motors(struct control_system *am)
 
 static void control_system_init_distance_angle(struct control_system *am)
 {
-  ausbee_pid_init(&(am->pid_distance), 2.5, 0.5, 0.5);
-  ausbee_pid_init(&(am->pid_angle),    3, 0.05, 0); // 2, 0.07, 0.1
+  ausbee_pid_init(&(am->pid_distance), 0.04, 0.0005, 0);
+  ausbee_pid_init(&(am->pid_angle),    0.02, 0.0005, 0);
 
   ausbee_pid_set_output_range(&(am->pid_distance), -100, 100);
   ausbee_pid_set_output_range(&(am->pid_angle),  -100, 100);
